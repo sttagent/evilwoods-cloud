c4a0f7acfa1e8baa25838e25fdcf2fe27986b287
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-03-15 13:01:03 +0100

    remove xonsh builder

--- bootc/evilwoods-server/scripts/install-binaries.bash
+++ bootc/evilwoods-server/scripts/install-binaries.bash
@@ -16,27 +16,28 @@ zellij_archive="zellij-x86_64-unknown-linux-musl.tar.gz"
 lego_version="4.21.0"
 lego_archive="lego_v${lego_version}_linux_amd64.tar.gz"
 
-mkdir -p usr/local/bin
 wget -q "https://github.com/bitwarden/sdk-sm/releases/download/bws-v${bitwarden_version}/${bitwarden_archive}"
 unzip $bitwarden_archive
-cp bws usr/local/bin/
+cp bws /usr/local/bin/
 
 # Install atuin
 wget -q "https://github.com/atuinsh/atuin/releases/latest/download/${atuin_archive_name}.tar.gz"
 tar -xvf "${atuin_archive_name}.tar.gz"
-cp  ${atuin_archive_name}/atuin usr/local/bin/
+cp  ${atuin_archive_name}/atuin /usr/local/bin/
 
 # Install chezmoi
 wget -q "https://github.com/twpayne/chezmoi/releases/download/v${chezmoi_version}/${chezmoi_archive}"
 tar -x chezmoi -f $chezmoi_archive
-cp chezmoi usr/local/bin/
+cp chezmoi /usr/local/bin/
 
 #install zellij
 wget -q "https://github.com/zellij-org/zellij/releases/latest/download/${zellij_archive}"
 tar -xf $zellij_archive
-cp zellij usr/local/bin/
+cp zellij /usr/local/bin/
 
 # install lego (letsencrypt)
 wget  "https://github.com/go-acme/lego/releases/download/v${lego_version}/${lego_archive}"
 tar -xvf $lego_archive
-cp lego usr/local/bin/
+cp lego /usr/local/bin/
+
+rm -rf *
