9e99dff3a7569ab6fb5b8e6d79b0bf69569f85c9
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-06-05 10:27:28 +0200

    remove cloudflared package
    podman image is used instead.

--- bootc/evilwoods-server/scripts/install-packages.bash
+++ bootc/evilwoods-server/scripts/install-packages.bash
@@ -7,5 +7,4 @@ dnf -y install rsync fish qemu-guest-agent firewalld \
     cockpit cockpit-podman cockpit-ostree unzip wget policycoreutils-python-utils
 dnf config-manager -y --add-repo https://pkgs.tailscale.com/stable/centos/10/tailscale.repo
 dnf -y install tailscale
-dnf -y install https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-x86_64.rpm
 dnf clean all
