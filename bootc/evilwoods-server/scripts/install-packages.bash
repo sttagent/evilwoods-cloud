d4d4065d2702459c969880e115db0d8184d33de0
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-07-06 13:14:13 +0200

    install restic backup tool

--- bootc/evilwoods-server/scripts/install-packages.bash
+++ bootc/evilwoods-server/scripts/install-packages.bash
@@ -4,6 +4,6 @@ set -euo pipefail
 
 dnf install -y dnf5-plugins
 dnf config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
-dnf -y install rsync qemu-guest-agent firewalld \
+dnf -y install rsync qemu-guest-agent firewalld restic \
     cockpit cockpit-podman cockpit-ostree unzip wget xonsh tailscale
 dnf clean all
