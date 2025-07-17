9e546fcf79dbba70c9c2c2249a06157babc5785c
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-07-17 10:15:00 +0200

    install filen package

--- bootc/evilwoods-desktop/scripts/install-packages.bash
+++ bootc/evilwoods-desktop/scripts/install-packages.bash
@@ -2,7 +2,10 @@
 
 set -euo pipefail
 
+mkdir -p /var/opt
+
 dnf -y config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+dnf -y install https://cdn.filen.io/@filen/desktop/release/latest/Filen_linux_x86_64.rpm
 # dnf -y copr enable pgdev/ghostty && dnf -y install ghostty
 dnf -y install fish neovim tailscale ripgrep fd-find bat just
 dnf -y remove firefox firefox-langpacks
