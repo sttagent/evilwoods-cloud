9fe0b13876f3e4183fcbc74ce6a843e640e97611
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-07-18 16:51:47 +0200

    quote string with cammand expansion

--- bootc/evilwoods-desktop/scripts/install-packages.bash
+++ bootc/evilwoods-desktop/scripts/install-packages.bash
@@ -5,9 +5,9 @@ set -euo pipefail
 mkdir -p /var/opt
 
 dnf -y config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
-dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-    https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf -y swap mesa-va-drivers mesa-va-drivers-freeworld
+dnf -y install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+  "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+Dnf -y swap mesa-va-drivers mesa-va-drivers-freeworld
 dnf -y swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
 dnf -y install https://cdn.filen.io/@filen/desktop/release/latest/Filen_linux_x86_64.rpm
 # dnf -y copr enable pgdev/ghostty && dnf -y install ghostty
