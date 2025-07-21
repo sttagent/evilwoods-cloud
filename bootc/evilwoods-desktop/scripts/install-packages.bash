276d189dc8e1b31ed65bb129585da777532f6584
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-07-21 17:06:03 +0200

    add missing packages

--- bootc/evilwoods-desktop/scripts/install-packages.bash
+++ bootc/evilwoods-desktop/scripts/install-packages.bash
@@ -11,6 +11,6 @@ dnf -y swap mesa-va-drivers mesa-va-drivers-freeworld
 dnf -y swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
 dnf -y install https://cdn.filen.io/@filen/desktop/release/latest/Filen_linux_x86_64.rpm
 # dnf -y copr enable pgdev/ghostty && dnf -y install ghostty
-dnf -y install fish neovim tailscale ripgrep fd-find bat just
+dnf -y install fish neovim tailscale ripgrep fd-find bat just fzf gnome-shell-extension-appindicator zoxide
 dnf -y remove firefox firefox-langpacks
 dnf -y clean all
