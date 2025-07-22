41e3e18a12e37da16226aece996c1c0e13a4a582
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-07-22 16:13:44 +0200

    remove filen installation
    Filen fails silently to install. I am using commandline util and systemd
    service instead.

--- bootc/evilwoods-desktop/scripts/install-packages.bash
+++ bootc/evilwoods-desktop/scripts/install-packages.bash
@@ -9,7 +9,6 @@ dnf -y install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-relea
   "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 dnf -y swap mesa-va-drivers mesa-va-drivers-freeworld
 dnf -y swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
-dnf -y install https://cdn.filen.io/@filen/desktop/release/latest/Filen_linux_x86_64.rpm
 # dnf -y copr enable pgdev/ghostty && dnf -y install ghostty
 dnf -y install fish neovim tailscale ripgrep fd-find bat just fzf gnome-shell-extension-appindicator zoxide
 dnf -y remove firefox firefox-langpacks
