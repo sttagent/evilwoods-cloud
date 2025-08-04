68e65eef860b022add50ec61818026d863fd210c
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-08-04 11:14:20 +0200

    install fail2ban on server

--- bootc/evilwoods-server/scripts/install-packages.bash
+++ bootc/evilwoods-server/scripts/install-packages.bash
@@ -5,5 +5,5 @@ set -euo pipefail
 dnf install -y dnf5-plugins
 dnf config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
 dnf -y install rsync qemu-guest-agent firewalld restic \
-    cockpit cockpit-podman cockpit-ostree unzip wget fish tailscale
+  cockpit cockpit-podman cockpit-ostree unzip wget fish tailscale fail2ban
 dnf clean all
