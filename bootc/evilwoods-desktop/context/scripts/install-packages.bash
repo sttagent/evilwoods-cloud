d962c036b17bde07db284b80e386cd48facbe90e
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-08-25 08:58:48 +0200

    copy config files in a new layer

--- bootc/evilwoods-desktop/context/scripts/install-packages.bash
+++ bootc/evilwoods-desktop/context/scripts/install-packages.bash
@@ -7,14 +7,10 @@ function evildnf {
     --setopt=keepcache=true "$@"
 }
 
-cp --no-target-directory -vR /context/config/usr /usr
-
 curl -fsSL "https://copr.fedorainfracloud.org/coprs/scottames/ghostty/repo/fedora-$(rpm -E %fedora)/scottames-ghostty-fedora-$(rpm -E %fedora).repo" |
   sudo tee /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:scottames:ghostty.repo >/dev/null
 
 mkdir -p /var/opt
-
-cp -vR /usr/etc/yum.repos.d /etc
 
 evildnf install \
   "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
